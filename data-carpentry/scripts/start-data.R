3b37e5f792c03d79f7a976e29f285b6181cccf64
bbartholdy <b.p.bartholdy@tudelft.nl>
2023-10-12 16:36:24 +0200

    gitautosync update 15

--- data-carpentry/scripts/start-data.R
+++ data-carpentry/scripts/start-data.R
@@ -1,5 +1,7 @@
-download.file(
-  "https://raw.githubusercontent.com/datacarpentry/r-socialsci/main/episodes/data/SAFI_clean.csv",
-  "data/SAFI_clean.csv", mode = "wb"
-)
+# If you need to download the file, uncomment the code from lines 3-6
+
+#download.file(
+#  "https://raw.githubusercontent.com/datacarpentry/r-socialsci/main/episodes/data/SAFI_clean.csv",
+#  "data/SAFI_clean.csv", mode = "wb"
+#)
 
