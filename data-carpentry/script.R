8e222348d7222afb364a3901ea978cee6f5cf251
bbartholdy <b.p.bartholdy@tudelft.nl>
2023-10-12 14:26:54 +0200

    gitautosync update 2

--- data-carpentry/script.R
+++ data-carpentry/script.R
@@ -1,3 +1,17 @@
 dir.create("data")
 dir.create("data_output")
 dir.create("fig_output")
+# this is a comment
+
+# Introduction to R
+3 + 5
+12 / 7
+pi
+area_hectares <- 1.0
+area_hectares
+area_hectares <- 2.0
+area_hectares 
+1.0 -> area_hectares  # possible but uncommon
+(area_hectares <- 1.0)  # assign and print at once
+2.47 * area_hectares
+(area_acres <- area_hectares * 2.47)
