f1f82dbdd89d21151d672ae67c3a79056c49807d
Aleksandra Wilczynska <a.e.wilczynska@tudelft.nl>
2023-10-13 10:20:39 +0200

    gitautosync update 1

---
+++ data-carpentry/scripts/data-wrangling.R
@@ -0,0 +1,14 @@
+## load the tidyverse
+library(tidyverse)
+library(here)
+
+# If you haven't downloaded the file:
+#download.file("https://ndownloader.figshare.com/files/11492171",
+#              "data/SAFI_clean.csv", mode = "wb")
+
+
+# Assign data to the object
+interviews <- read_csv(here("data", "SAFI_clean.csv"), na = "NULL")
+
+# preview the data
+# view(interviews)
