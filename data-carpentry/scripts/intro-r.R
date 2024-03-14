8ccb40f5142dfb90ec7aaa277e494dec9980621e
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2024-03-14 09:48:46 +0100

    gitautosync update 3

--- data-carpentry/scripts/intro-r.R
+++ data-carpentry/scripts/intro-r.R
@@ -1,1 +1,2 @@
-download.file("https://raw.githubusercontent.com/datacarpentry/r-socialsci/main/episodes/data/SAFI_clean.csv", "data/SAFI_clean.csv", mode = "wb")
+#download.file("https://raw.githubusercontent.com/datacarpentry/r-socialsci/main/episodes/data/SAFI_clean.csv", "data/SAFI_clean.csv", mode = "wb")
+
