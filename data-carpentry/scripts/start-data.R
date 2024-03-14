f17d7f781d2a2ddcf089fdcacddb146b6a552e33
Stephanie van de Sandt <102951422+vansteph@users.noreply.github.com>
2024-03-14 15:55:19 +0100

    Update start-data.R

--- data-carpentry/scripts/start-data.R
+++ data-carpentry/scripts/start-data.R
@@ -8,3 +8,19 @@ interviews
 
 #Size of data frame
 head(interviews) #show the first six rows
+
+dim(interviews) #dimensions
+nrow(interviews) #rows
+ncol(interviews) #columns
+
+#content of data frame
+head(interviews) #show the first 6 rows
+tail(interviews) #Show the last 6 rows
+
+#Names of columns
+names(interviews)
+
+#Data summaries
+str(interviews) #structure of object and information about the class
+summary(interviews) #summary statistics for each column
+glimpse(interviews)
