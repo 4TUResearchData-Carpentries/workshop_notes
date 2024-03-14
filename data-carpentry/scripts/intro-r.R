7875aa091b09de9bf2ba63e106f25f79fc0cb154
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2024-03-14 10:08:45 +0100

    gitautosync update 7

--- data-carpentry/scripts/intro-r.R
+++ data-carpentry/scripts/intro-r.R
@@ -1,10 +1,11 @@
 #download.file("https://raw.githubusercontent.com/datacarpentry/r-socialsci/main/episodes/data/SAFI_clean.csv", "data/SAFI_clean.csv", mode = "wb")
 
+# land area in hectares
 area_hectares <- 1.0
 
-area_hectares * 2.47
+area_hectares * 2.47 
 
-area_hectares <- 2.5
+area_hectares <- 2.5 # change value of area
 
 area_hectares * 2.47
 
