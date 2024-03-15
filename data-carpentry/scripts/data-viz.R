fc16b526d7b1867a494c22dfd4bd794b22fb1d94
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2024-03-15 15:12:34 +0100

    gitautosync update 34

--- data-carpentry/scripts/data-viz.R
+++ data-carpentry/scripts/data-viz.R
@@ -1,2 +1,10 @@
 # Data visualisation
+library(tidyverse)
 
+interviews_plotting <- read_csv("data_output/interviews_plotting.csv")
+
+# first plot
+
+interviews_plotting %>%
+  ggplot(aes(x = no_membrs, y = number_items)) +
+    geom_point(alpha  = 0.5)
