4680d319560c84ec70259bb984e5a3953ffb4f3c
peterverhaar <peter.verhaar@gmail.com>
2024-03-15 10:35:17 +0100

    gitautosync update 5

--- data-carpentry/scripts/data-wrangling.R
+++ data-carpentry/scripts/data-wrangling.R
@@ -5,3 +5,12 @@ library(here)
 interviews <- read_csv(
   here("data", "SAFI_clean.csv"), 
   na = "NULL")
+
+## select the village column
+select( interviews , village  )
+
+## select the village and the no_membrs column
+select( interviews , village, no_membrs  )
+
+## select the columns village and rooms and all columns in between 
+interviews2 <- select( interviews , village:rooms )
