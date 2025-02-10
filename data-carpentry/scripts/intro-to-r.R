760d6b9b1f5ceaca9bd407a0e6e65969220d9c28
eduardklap <et.klapwijk@gmail.com>
2025-02-10 10:21:41 +0100

    gitautosync update 17

--- data-carpentry/scripts/intro-to-r.R
+++ data-carpentry/scripts/intro-to-r.R
@@ -23,4 +23,8 @@ area_hectares # typing the name of the object it prints the value
 
 2.47 * area_hectares # convert area into acres
 area_hectares <- 10 # change the value of area_hectares
+2.47 * area_hectares # convert into acres again
+
+
+
 
