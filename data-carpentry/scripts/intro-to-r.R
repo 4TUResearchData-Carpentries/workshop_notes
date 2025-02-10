65f27d294ea1ccecec9fbfa3a98d8cc7b036b585
eduardklap <et.klapwijk@gmail.com>
2025-02-10 09:57:52 +0100

    gitautosync update 8

--- data-carpentry/scripts/intro-to-r.R
+++ data-carpentry/scripts/intro-to-r.R
@@ -8,3 +8,7 @@ dir.create("scripts")
 # We are going to directly download the data from the Figshare repository
 download.file("https://ndownloader.figshare.com/files/11492171",
               "data/SAFI_clean.csv", mode = "wb")
+?download.file
+
+### Installing packages
+
