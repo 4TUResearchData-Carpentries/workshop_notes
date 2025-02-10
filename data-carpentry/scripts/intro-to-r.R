d515e36ec3f191e131f28e104fc5978eb36437a8
eduardklap <et.klapwijk@gmail.com>
2025-02-10 10:56:59 +0100

    gitautosync update 26

--- data-carpentry/scripts/intro-to-r.R
+++ data-carpentry/scripts/intro-to-r.R
@@ -48,4 +48,9 @@ round(digits = 2, x = 3.14159) # we can switch the order if we name the argument
 floor(1.624)
 
 # Vectors and data types
+hh_members <- c(3, 7, 10, 6) # using the c() we create a vector of 4 numbers
+hh_members
+respondent_wall_type <- c("cement", "brick", "wood") # create a vector of 3 character elements
+respondent_wall_type
 
+length(hh_members) # return the number of elements in the hh_members vector
