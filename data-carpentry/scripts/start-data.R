3235b6fd7d97ded244cd595b271faab93917fe6d
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2025-02-10 16:12:25 +0100

    gitautosync update 5

--- data-carpentry/scripts/start-data.R
+++ data-carpentry/scripts/start-data.R
@@ -24,11 +24,11 @@ interviews[c(1,3,4), 3]
 interviews[3, ] # output as tibble
 interviews[, 3]
 interviews[, -1]
-interviews[-c(1,3,4), ] # index everything EXCEPT rows 1, 3, and 4
+interviews[-c(1,3,4), ] # subset everything EXCEPT rows 1, 3, and 4
 interviews["village"] # name of variable to subset as tibble
 interviews[, c("village", "no_meals")]
 interviews[["village"]] # output as vector
-interviews$respondent_wall_type[1:3]
+interviews$respondent_wall_type[1:3] # subset the first 3 elements of the respondent_wall_type variable
 
 ## Exercise 1
 
