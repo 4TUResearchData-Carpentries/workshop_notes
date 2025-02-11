a1169351003ae673b488f3b8cd0c9c9b6278b6e0
peterverhaar <peter.verhaar@gmail.com>
2025-02-11 14:41:19 +0100

    gitautosync update 9

--- data-carpentry/scripts/data-viz.R
+++ data-carpentry/scripts/data-viz.R
@@ -5,7 +5,15 @@ library(here)
 
 interviews_plotting <- read_csv(here("data_output","interview_plotting.csv"))
 
+## A basic scatter plot
 interviews_plotting %>%
   ggplot(aes(x=no_membrs,y=number_items)) +
   geom_point()
+
+# Making points transparent
+interviews_plotting %>%
+  ggplot(aes(x=no_membrs,y=number_items)) +
+  geom_point()
+
+
 
