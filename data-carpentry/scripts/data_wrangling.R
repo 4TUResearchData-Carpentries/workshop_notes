1de925e903efc622ae97c9ce0a6c46fb1c418047
peterverhaar <peter.verhaar@gmail.com>
2025-02-11 09:33:03 +0100

    gitautosync update 10

--- data-carpentry/scripts/data_wrangling.R
+++ data-carpentry/scripts/data_wrangling.R
@@ -11,3 +11,5 @@ interviews <- read_csv( here("data","SAFI_clean.csv") , na="NULL" )
 select( interviews , village )
 select( interviews , village, no_membrs , rooms )
 interviews2 <- select( interviews , village:memb_assoc )
+
+## select columns starting
