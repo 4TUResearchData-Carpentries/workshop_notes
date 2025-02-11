cf9004cd4eeca5e7ba83e89c0b4f998da9f1f9d0
peterverhaar <peter.verhaar@gmail.com>
2025-02-11 14:44:43 +0100

    gitautosync update 12

--- data-carpentry/scripts/data-viz.R
+++ data-carpentry/scripts/data-viz.R
@@ -13,7 +13,7 @@ interviews_plotting %>%
 # Making points transparent and add colour
 interviews_plotting %>%
   ggplot(aes(x=no_membrs,y=number_items)) +
-  geom_point(alpha=0.5)
+  geom_point(alpha=0.5, colour = '#7d1542')
 
 
 
