31c8da85a13ae3960e7128c167293c625a340830
peterverhaar <peter.verhaar@gmail.com>
2025-02-11 14:40:05 +0100

    gitautosync update 8

--- data-carpentry/scripts/data-viz.R
+++ data-carpentry/scripts/data-viz.R
@@ -5,5 +5,7 @@ library(here)
 
 interviews_plotting <- read_csv(here("data_output","interview_plotting.csv"))
 
-
+interviews_plotting %>%
+  ggplot(aes(x=no_membrs,y=number_items)) +
+  geom_point()
 
